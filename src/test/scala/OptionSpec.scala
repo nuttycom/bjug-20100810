6e02fe2713c2117ef086f078549cfd6358c72f7a
Kris Nuttycombe <kris@socialmedia.com>
2010-08-10 17:21:20 -0600

    prepped for live session

--- src/test/scala/OptionSpec.scala
+++ src/test/scala/OptionSpec.scala
@@ -7,48 +7,23 @@ import org.scalacheck.Arbitrary.arbitrary
 import scala.collection.JavaConversions._
 
 trait PeopleGen {
-  val femaleNameGen = oneOf("Sophie","Ada","Grace","Barbara")
-  val maleNameGen   = oneOf("Scott","Fred","Venkat","Daniel")
-
-
-  def maybeGen[T](f1: Int, f2: Int, someGen: Gen[T]): Gen[T] = {
-    frequency((f1, someGen), (f2, value[T](null.asInstanceOf[T])))
-  }
-
-  val addressGen = for {
-    addr1 <- alphaStr
-    addr2 <- maybeGen(1, 1, alphaStr)
-    city  <- oneOf("Denver","Boulder")
-    state <- value("Colorado")
-    zip   <- containerOfN[List, Int](5, choose(0, 9)).map(_.mkString)
-  } yield new Address(addr1, addr2, city, state, zip)
-
-  def personGen(nameGen: Gen[String] = frequency((1, maleNameGen), (1, femaleNameGen))): Gen[Person] = for {
-    name    <- nameGen
-    father  <- maybeGen(1, 3, personGen(maleNameGen))
-    mother  <- maybeGen(1, 3, personGen(femaleNameGen))
-    address <- maybeGen(9, 1, addressGen)
-  } yield new Person(name, father, mother, address)
-
-  implicit val arbPerson = Arbitrary(personGen())
 }
 
 object PersonProps extends Properties("Person") with PeopleGen {
-  property("tautology") = forAll((p: Person) => p.mother.father.address.addr2 == "123 45th St." || p.mother.father.address.addr2 != "123 45th St.")
 }
 
-//object JOptSpec extends Properties("JOpt") with OptGen {
-//  property("non-emptiness") = forAll((v: JOpt[Any]) => v.isInstanceOf[JOpt.Some[_]] ==> !v.isEmpty) 
-//  property("emptiness")     = forAll((v: JOpt[Any]) => v.isInstanceOf[JOpt.None[_]] ==> v.isEmpty )
-//
-//  property("iterability") = forAll(
-//    (i: Int) => i > Integer.MIN_VALUE ==> {
-//      var extracted = Integer.MIN_VALUE
-//      for (ii <- JOpt.some(i)) extracted = ii
-//      extracted == i
-//    }
-//  ) 
-//}
+object JOptSpec extends Specification {
+}
+
+
+
+
+
+
+
+
+
+
 
 //object PersonSpec extends Specification with ScalaCheck with PeopleGen {
 //  val people = containerOfN[List, Person](1000, personGen()).sample.get
